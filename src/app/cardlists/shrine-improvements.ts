eaf91c80fb8eb5d7ba5e0fddc23c7b52c0189a92
Jonathan Croxton <jccroxton@gmail.com>
2024-07-04 12:23:27 -0400

    add remaining cards (still need to clean up)

--- src/app/cardlists/shrine-improvements.ts
+++ src/app/cardlists/shrine-improvements.ts
@@ -1,20 +1,98 @@
-export let shrine_improvements = [
-'arsenal-1',
-'gardens-1',
-'greathall-1',
-'hiddenpassageways-1',
-'infirmary-1',
-'innersanctum-1',
-'intruderalarm-1',
-'kennels-1',
-'library-1',
-'observationpoint-1',
-'researchfacility-1',
-'secretacademy-1',
-'sparringring-1',
-'spikedwalls-1',
-'thickwalls-1',
-'throneroom-1',
-'trainingfacility-1',
-'warroom-1',
-]
+// add stats
+class ShrineImprovement {
+  name: string;
+  filename: string;
+  text: string;
+}
+const shrineImprovements: ShrineImprovement[] = [
+  {
+    name: 'Arsenal',
+    filename: 'arsenal-1.png',
+    text: '© : You may play Items as if they were Continuous Events until end of turn.',
+  },
+  {
+    name: 'Gardens',
+    filename: 'gardens-1.png',
+    text: '© : Surge 1.',
+  },
+  {
+    name: 'Great Hall',
+    filename: 'greathall-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Knight: Target © Knight gains +1Spd and +1Pwr until end of turn.',
+  },
+  {
+    name: 'Hidden Passageways',
+    filename: 'hiddenpassageways-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Assassin or © Tactician: Deal 1 damage to target © Shrine.',
+  },
+  {
+    name: 'Infirmary',
+    filename: 'infirmary-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Cleric: Heal 2 damage from target © Unit.',
+  },
+  {
+    name: 'Inner Sanctum',
+    filename: 'innersanctum-1.png',
+    text: '© : All non-Continuous Events you control gain +1Pwr until end of turn.',
+  },
+  {
+    name: 'Intruder Alarm',
+    filename: 'intruderalarm-1.png',
+    text: '© : Target © Unit loses Stealth until end of turn.',
+  },
+  {
+    name: 'Kennels',
+    filename: 'kennels-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Soldier with cost 3 or greater: Place a “War Kitten” token into your combat zone.',
+  },
+  {
+    name: 'Library',
+    filename: 'library-1.png',
+    text: '© : Draw a Card then discard a Card.',
+  },
+  {
+    name: 'Observation Point',
+    filename: 'observationpoint-1',
+    text: '',
+  },
+  {
+    name: 'War Room',
+    filename: 'warroom-1.png',
+    text: '© : Increase your starting hand size by 1.',
+  },
+  {
+    name: 'Research Facility',
+    filename: 'researchfacility-1.png',
+    text: '© : Once per turn, during an opponent’s action, you may play a non-Continuous Event with 0 Spd as if it had 1 Spd.',
+  },
+  {
+    name: 'Secret Academy',
+    filename: 'secretacademy-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Unit with Stealth and cost 3 or greater: Deal 1 damage to target © Unit if it doesn’t have Stealth.',
+  },
+  {
+    name: 'Sparring Ring',
+    filename: 'sparringring-1.png',
+    text: '© : Use this ability once per turn; Exhaust a © Barbarian or © Warrior: Exhaust target non-Epic © Unit.',
+  },
+  {
+    name: 'Spiked Walls',
+    filename: 'spikedwalls-1.png',
+    text: '© : When a Unit deals combat damage to this Card for the first time each turn, deal 1 damage to that Unit.',
+  },
+  {
+    name: 'Thick Walls',
+    filename: 'thickwalls-1.png',
+    text: '',
+  },
+  {
+    name: 'Throne Room',
+    filename: 'throneroom-1.png',
+    text: '© : All © Units you control with cost 5 or greater gain +1Pwr.',
+  },
+  {
+    name: 'Training Facility',
+    filename: 'trainingfacility-1.png',
+    text: '© : Target © Unit gains +1Pwr until end of turn.',
+  },
+];
--- src/app/cardlists/shrines.ts
+++ src/app/cardlists/shrines.ts
@@ -1,8 +1,75 @@
-export let shrines = [
-'hallofshadowscrookedcornerstone',
-'hallowedgroundstrangersfirstsighting',
-'mikiriravinefirststronghold',
-'templeintheskiespilgrimageterminus',
-'thegreatoakfabledguardian',
-'unseencitydmarquecontrol',
-]
+import { Element } from './base-cards';
+// add epic rarity
+class Shrine {
+  name: string;
+  filename: string;
+  type: Element;
+  hp: number;
+  power: number;
+  speed: number;
+  text: string;
+  artist: string;
+}
+
+export let shrines: Shrine[] = [
+  {
+    name: 'Hall of Shadows - Crooked Cornerstone',
+    filename: 'hallofshadowscrookedcornerstone',
+    type: Element.Dark,
+    hp: 19,
+    power: 0,
+    speed: 0,
+    text: 'When you resolve a Dark Card that was played as the first action of the turn, deal 1 damage to all Enemy Structures.',
+    artist: 'Isiah Xavier Bradley',
+  },
+  {
+    name: 'Hallowed Ground - Stranger’s First Sighting',
+    filename: 'hallowedgroundstrangersfirstsighting',
+    type: Element.Light,
+    hp: 25,
+    power: 0,
+    speed: 0,
+    text: 'When a Light Structure or Light Item enters your combat zone for the first time each turn, heal 1 damage from this Card.',
+    artist: 'Fernando Cordeiro',
+  },
+  {
+    name: 'Mikiri Ravine - First Stronghold',
+    filename: 'mikiriravinefirststronghold',
+    type: Element.Fire,
+    hp: 17,
+    power: 0,
+    speed: 0,
+    text: 'When a Fire Unit enters your combat zone for the first time each turn, take an additional attack action or pass action.',
+    artist: 'Arturo Gomez Martinez',
+  },
+  {
+    name: 'Temple in the Skies - Pilgrimage Terminus',
+    filename: 'templeintheskiespilgrimageterminus',
+    type: Element.Air,
+    hp: 21,
+    power: 0,
+    speed: 0,
+    text: 'When an Air Unit enters your combat zone for the first time each turn, draw a Card then discard a Card.',
+    artist: 'Kateryna Vitkovska',
+  },
+  {
+    name: 'The Great Oak - Fabled Guardian',
+    filename: 'thegreatoakfabledguardian',
+    type: Element.Earth,
+    hp: 21,
+    power: 0,
+    speed: 0,
+    text: 'When you play an Earth Event for the first time each turn, this Card gains Armor 1 until end of turn.',
+    artist: 'Megan Toole',
+  },
+  {
+    name: "Unseen City - D'Marque Control",
+    filename: 'unseencitydmarquecontrol',
+    type: Element.Water,
+    hp: 20,
+    power: 0,
+    speed: 0,
+    text: 'When a Water Unit with Stealth enters your combat zone for the first time each turn, you may deal 1 damage to target Unit.',
+    artist: 'James McCully',
+  },
+];
