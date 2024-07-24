8cc9b524aaedc8b9545015fd7f15dd12d93cfa86
Jonathan Croxton <jccroxton@gmail.com>
2024-07-23 23:17:40 -0400

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/deckbuilder',
+  // output: 'export',
+  // images: { unoptimized: true },
+  // basePath: '/deckbuilder',
   typescript: {
     ignoreBuildErrors: true,
   },
